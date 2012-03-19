d1a342770d360071159cdcbed84f3522620d8821
AJ Reynolds <stampedeboss@gmail.com>
2012-03-19 17:34:36 +0000

    Added new utility and fixed issues with other modules
    git-svn-id: file:///srv/Shared/Programming/svn/trunk@78 b37ba28c-1ce9-4bc8-a78a-c9b2a1b2d1f0

--- daddyvision/library/dbclean.py
+++ daddyvision/library/dbclean.py
@@ -18,6 +18,7 @@ import sqlite3
 import sys
 import tempfile
 import time
+import re
 
 __pgmname__ = 'dbclean'
 __version__ = '$Rev$'
@@ -47,11 +48,17 @@ fileparser = FileParser()
 db = sqlite3.connect(config.DBFile)
 cursor = db.cursor()
 
-class CleanDatabase(object):
-    def __init__(self):
-        pass
+directory_old = '/mnt/TV/Series'
 
-    def ScanDeadEntries(self):
+re_dir_old = re.compile('^{}.*$'.format(directory_old), re.IGNORECASE)
+re_dir_current = re.compile('^{}.*$'.format(config.SeriesDir), re.IGNORECASE)
+
+
+class CleanDatabase(object):
+    def __init__(self, config):
+        self.config = config
+
+    def ScanFileEntries(self):
         log.trace('ScanSeriesLibrary: Start')
 
         cursor.execute('SELECT COUNT(*)e FROM Files')
@@ -60,37 +67,98 @@ class CleanDatabase(object):
 
         Files_Processed = 0
         Files_Deleted = 0
+        Files_Updated = 0
 
         log.info('Number of File to be Checked: %s' % File_Count)
 
         cursor.execute('SELECT f.FileName FROM Files f')
-
-        for row in cursor:
+        Result = cursor.fetchall()
+        for row in Result:
             Files_Processed += 1
             if os.path.exists(row[0]):
                 pass
             else:
-                log.info('Entry Removed: {}'.format(row[0]))
-                try:
-                    cursor.execute('DELETE FROM Files WHERE FileName="{}"'.format(row[0]))
-                    Files_Deleted += 1
-                except sqlite3.Error, e:
-                    raise UnexpectedErrorOccured("File Information Insert: {} {}".format(e, row[0]))
-
-                quotient, remainder = divmod(Files_Processed, 250)
-                if remainder == 0:
-                    db.commit()
-                    log.info('Files Checked: %2.2f%% - %5s of %5s   Number Deleted: %s' % (Files_Processed/ File_Count,
-                                                                                             Files_Processed,
-                                                                                             File_Count,
-                                                                                             Files_Deleted
-                                                                                             )
+                _target = re.split(directory_old, row[0])
+                if len(_target) > 1:
+                    try:
+                        Files_Updated += 1
+                        episode = os.path.join(self.config.SeriesDir, _target[1].lstrip(os.sep))
+                        cursor.execute('UPDATE Files SET FileName="{}" WHERE FileName="{}"'.format(episode, row[0]))
+                    except  sqlite3.Error, e:
+                        print 'ERROR: {}'.format(e)
+                else:
+                    log.info('Entry Removed: {}'.format(row[0]))
+                    try:
+                        cursor.execute('DELETE FROM Files WHERE FileName="{}"'.format(row[0]))
+                        Files_Deleted += 1
+                    except sqlite3.Error, e:
+                        raise UnexpectedErrorOccured("File Information Insert: {} {}".format(e, row[0]))
+    
+            quotient, remainder = divmod(Files_Processed, 250)
+            if remainder == 0:
+                db.commit()
+                log.info('Files Checked: %2.2f%% - %5s of %5s   Number Deleted: %s   Number Updated: %s' % (Files_Processed/ File_Count,
+                                                                                                            Files_Processed,
+                                                                                                            File_Count,
+                                                                                                            Files_Deleted,
+                                                                                                            Files_Updated
+                                                                                         )
                          )
         db.commit()
         log.info('Complete: Files Checked: %5s   Number Deleted: %s' % (Files_Processed,
                                                                           Files_Deleted
                                                                           ))
 
+    def ScanDownloadEntries(self):
+        log.trace('ScanSeriesLibrary: Start')
+
+        cursor.execute('SELECT COUNT(*)e FROM Downloads')
+        Result = cursor.fetchall()
+        File_Count = Result[0][0]
+
+        Files_Processed = 0
+        Files_Deleted = 0
+        Files_Updated = 0
+
+        log.info('Number of File Entries to be Checked: %s' % File_Count)
+
+        cursor.execute('SELECT d.Name, d.FileName FROM Downloads d')
+        Result = cursor.fetchall()
+        for row in Result:
+            Files_Processed += 1
+            if os.path.exists(row[1]):
+                pass
+            else:
+                _target = re.split(directory_old, row[1])
+                if len(_target) > 1:
+                    try:
+                        Files_Updated += 1
+                        episode = os.path.join(self.config.SeriesDir, _target[1].lstrip(os.sep))
+                        cursor.execute('UPDATE Downloads SET FileName="{}" WHERE Name="{}" and FileName="{}"'.format(episode, row[0], row[1]))
+                    except  sqlite3.Error, e:
+                        print 'ERROR: {}'.format(e)
+                else:
+                    log.info('Entry Removed: {}  {}'.format(row[0], row[1]))
+                    try:
+                        cursor.execute('DELETE FROM Downloads WHERE Name="{}" and FileName="{}"'.format(row[0], row[1]))
+                        Files_Deleted += 1
+                    except sqlite3.Error, e:
+                        raise UnexpectedErrorOccured("File Information Insert: {} {}".format(e, row[0]))
+    
+            quotient, remainder = divmod(Files_Processed, 250)
+            if remainder == 0:
+                db.commit()
+                log.info('Files Checked: %2.2f%% - %5s of %5s   Number Deleted: %s   Number Updated: %s' % (Files_Processed/ File_Count,
+                                                                                                            Files_Processed,
+                                                                                                            File_Count,
+                                                                                                            Files_Deleted,
+                                                                                                            Files_Updated
+                                                                                         )
+                         )
+        db.commit()
+        log.info('Complete: Files Checked: %5s   Number Deleted: %s' % (Files_Processed,
+                                                                          Files_Deleted
+                                                                          ))
 
 if __name__ == '__main__':
 
@@ -113,6 +181,6 @@ if __name__ == '__main__':
     log.debug("Parsed command line options: {!s}".format(options))
     log.debug("Parsed arguments: %r" % args)
 
-    library = CleanDatabase()
-    library.ScanDeadEntries()
-
+    library = CleanDatabase(config)
+#    library.ScanFileEntries()
+    library.ScanDownloadEntries()
