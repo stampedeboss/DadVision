e16b707b42b68730313915a9bac8bb56f8f65453
AJ Reynolds <stampedeboss@gmail.com>
2014-09-08 08:00:32 +0000

    Changes made to accomadate changes to the psutil modules.
    git-svn-id: file:///srv/Shared/Programming/svn/trunk@460 b37ba28c-1ce9-4bc8-a78a-c9b2a1b2d1f0

--- DadVision/dadvision/utilities/chkrun.py
+++ DadVision/dadvision/utilities/chkrun.py
@@ -30,54 +30,55 @@ __credits__ = []
 log = logging.getLogger(__pgmname__)
 
 class chkStatus(Common):
-    def __init__(self):
-        super(chkStatus, self).__init__()
+	def __init__(self):
+		super(chkStatus, self).__init__()
 
-    def check(self):
-        nameList = ['rsync', 'python2.7', 'python']
+	def check(self):
+		nameList = ['python2.7', 'python', 'flexget', 'deluge', 'rsync']
 
-        time.sleep(0.2)
+		time.sleep(0.2)
 
-        pidList = psutil.process_iter()
-        for p in pidList:
+		pidList = psutil.process_iter()
+		for p in pidList:
 			try:
-				if p.name in nameList:
-					cmdline = p.cmdline
-					if os.path.basename(cmdline[1]) == 'chkrun':
+				if p.name() in nameList:
+					cmdline = p.cmdline()[1]
+					if os.path.basename(p.cmdline()[1]) == 'chkrun':
 						continue
-					if p.name in nameList:
-						if p.name == "python2.7" and len(cmdline) > 2:
-							log.debug('%s %s' % (p.name, cmdline))
-						elif p.name == 'python':
-							if os.path.basename(cmdline[1]) == 'syncrmt':
-								continue
-							log.info('{:>6d} {:<8s} {} {}'.format(p.pid, p.terminal, os.path.basename(cmdline[1]), cmdline[1:]))
-						elif p.name == 'rsync':
-							log.info('{:>6d} {:<8s} {} {}'.format(p.pid, p.terminal, p.name, cmdline[-1]))
+					if os.path.basename(cmdline) == 'pydevd.py':
+						continue
+					if p.name() == "python2.7" and len(cmdline) > 2:
+						log.debug('%s %s' % (p.name, cmdline))
+					elif p.name() == 'python':
+						if os.path.basename(p.cmdline()[1]) == 'syncrmt':
+							continue
+						log.info('{:>6d} {:<8s} {} {}'.format(p.pid, p.terminal(), os.path.basename(cmdline), cmdline))
+					elif p.name == 'rsync':
+						log.info('{:>6d} {:<8s} {} {}'.format(p.pid, p.terminal(), p.name(), cmdline))
 			except:
 				pass
 
 
 if __name__ == '__main__':
 
-    logger.initialize()
-    common = chkStatus()
+	logger.initialize()
+	common = chkStatus()
 
-    Common.args = Common.options.parser.parse_args(sys.argv[1:])
-    log.debug("Parsed command line: {!s}".format(common.args))
+	Common.args = Common.options.parser.parse_args(sys.argv[1:])
+	log.debug("Parsed command line: {!s}".format(common.args))
 
-    log_level = logging.getLevelName(common.args.loglevel.upper())
+	log_level = logging.getLevelName(common.args.loglevel.upper())
 
-    if common.args.logfile == 'daddyvision.log':
-        log_file = '{}.log'.format(__pgmname__)
-    else:
-        log_file = os.path.expanduser(common.args.logfile)
+	if common.args.logfile == 'daddyvision.log':
+		log_file = '{}.log'.format(__pgmname__)
+	else:
+		log_file = os.path.expanduser(common.args.logfile)
 
-    # If an absolute path is not specified, use the default directory.
-    if not os.path.isabs(log_file):
-        log_file = os.path.join(logger.LogDir, log_file)
+	# If an absolute path is not specified, use the default directory.
+	if not os.path.isabs(log_file):
+		log_file = os.path.join(logger.LogDir, log_file)
 
-    logger.start(log_file, log_level, timed=True)
+	logger.start(log_file, log_level, timed=True)
 
-    common.check()
-    sys.exit(0)
+	common.check()
+	sys.exit(0)
